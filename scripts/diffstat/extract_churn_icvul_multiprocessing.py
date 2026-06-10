8f6f36112bfd084e21859b6c8cac7f1a39859b54
zaryanz <aryanx404@gmail.com>
2026-06-10 16:52:22 +0200

    fix: used full dataset in jupyter notebook

--- scripts/diffstat/extract_churn_icvul_multiprocessing.py
+++ scripts/diffstat/extract_churn_icvul_multiprocessing.py
@@ -10,7 +10,7 @@ from utils.diffstat import get_diffstat_metrics
 from utils.file import is_test_file, is_source_code
 
 # Config paths
-INPUT_CSV = "data/intermediate/commits_icvul_restricted.csv"
+INPUT_CSV = "data/intermediate/commits_icvul.csv"
 REPO_BASE_DIR = Path("repos")
 OUTPUT_PATH = "data/intermediate/churn_icvul.csv"
 
